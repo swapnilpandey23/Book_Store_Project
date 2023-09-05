874ddc99a4bd36fdad2ba90e8ff0b7669f8af5b5
Swapnil Pandey <swapnil.pandey60@gmail.com>
2023-09-05 20:21:34 +0530

    All basic CRUD operations API are functional

--- back-end/controllers/bookController.js
+++ back-end/controllers/bookController.js
@@ -25,6 +25,69 @@ const createNewBook = async (req, res) => {
   }
 };
 
+const getAllBooks = async (_req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json({ count: books.length, data: books });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Error: error.Message });
+  }
+};
+
+const getOneBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findById({ _id: id });
+    if (!book) {
+      res.status(404).send('Book not found!');
+    }
+    res.status(200).json({ Count: book.length, Data: book });
+  } catch (error) {
+    console.log(error.Message);
+    res.status(500).json({ Error: error.Message });
+  }
+};
+
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      // 400 -  Bad request code.
+      res.status(400).json({
+        Alert: `One or multiple fields missing, kindly input all the data.`,
+      });
+    } else {
+      const book = await Book.findByIdAndUpdate({ _id: id }, req.body);
+      if (!book) {
+        res.status(404).send('Book not found!');
+      }
+      res.status(201).json({ Data: book });
+    }
+  } catch (error) {
+    console.log(error.Message);
+    res.status(500).send({ Error: error.Message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByIdAndDelete({ _id: id });
+    if (!book) {
+      res.status(404).send(`Book not found!`);
+    }
+    res.status(200).json({ Deleted: book });
+  } catch (err) {
+    console.log(err.Message);
+    res.status(500).send({ Error: err.Message });
+  }
+};
+
 export default {
   createNewBook,
+  getAllBooks,
+  getOneBook,
+  updateBook,
+  deleteBook,
 };
--- back-end/routes/bookRoutes.js
+++ back-end/routes/bookRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 import bookController from '../controllers/bookController.js';
 
 router.post('/newBook', bookController.createNewBook);
-
+router.get('/allBooks', bookController.getAllBooks);
+router.get('/:id', bookController.getOneBook);
+router.put('/updateBook/:id', bookController.updateBook);
+router.delete('/:id', bookController.deleteBook);
 export default router;
